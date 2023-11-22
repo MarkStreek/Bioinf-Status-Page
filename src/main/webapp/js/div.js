51d310028ddd09bc34251292cd59473fd091408d
Coldbirdie <60214213+Coldbirdie@users.noreply.github.com>
2023-11-22 15:55:17 +0100

    Testing with padding and margins for div objects.

--- src/main/webapp/js/div.js
+++ src/main/webapp/js/div.js
@@ -1,4 +1,10 @@
-for (let i = 0; i < 30; i++) {
-    document.getElementById("innerdiv").
-        innerHTML += "<div class='col border border-1 border-primary m-1'>1</div>";
+const n = 200;
+const innerDiv = document.getElementById("innerdiv");
+for (let i = 0; i < n; i++) {
+    const newDivObject = document.createElement('div');
+    newDivObject.classList.add('col', 'border', 'border-1', 'border-primary');
+    newDivObject.textContent = '1';
+    newDivObject.style.margin = `${Math.sqrt(n)}px`
+    newDivObject.style.padding = `15px`;
+    innerDiv.appendChild(newDivObject);
 }
