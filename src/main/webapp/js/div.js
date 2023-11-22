13953b146fea42d7041f5961222ad0f5bdf6dc6f
Sibrenreekers <s.h.b.reekers@st.hanze.nl>
2023-11-22 13:33:50 +0100

    Changed welcome.html and div.js to show a div grid

--- src/main/webapp/js/div.js
+++ src/main/webapp/js/div.js
@@ -1,4 +1,4 @@
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 30; i++) {
     document.getElementById("innerdiv").
-        innerHTML += "<div class='col border border-1 border-primary'>Hello geeks</div>";
+        innerHTML += "<div class='col border border-1 border-primary m-1'>1</div>";
 }
