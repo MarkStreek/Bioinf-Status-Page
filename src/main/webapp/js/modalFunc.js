9448c45071877d2f7cd21d25e087cad8f2f2457c
Coldbirdie <l.t.stein@st.hanze.nl>
2023-12-06 13:54:09 +0100

    Added comments in modalFunc

--- src/main/webapp/js/modalFunc.js
+++ src/main/webapp/js/modalFunc.js
@@ -1,4 +1,5 @@
 function openModal(text) {
+    // Pop-over button open event
     document.querySelector('.overlay').style.display = "block";
     document.getElementById('pc-title').innerText = text.split('.')[0];
     document.getElementById('modal-text').innerText = text;
@@ -6,12 +7,14 @@ function openModal(text) {
 }
 
 function closeModal() {
+    // Pop-over inside close (cross) event
     document.getElementById('myModal').style.display = "none";
     document.querySelector('.overlay').style.display = "none";
 }
 
 // Sluit de modal als er buiten de modal-content wordt geklikt
 window.onclick = function(event) {
+    // Pop-over outside close event
     let modal = document.getElementById('myModal');
     if (event.target === modal) {
         closeModal();
