a6c437a9c531e434ac83ee624e8817242751e19c
mvandestreek <m.van.de.streek@st.hanze.nl>
2023-12-20 21:18:40 +0100

    Testing out some new rewriting...

--- src/main/webapp/js/createWorkStationCards.js
+++ src/main/webapp/js/createWorkStationCards.js
@@ -1,174 +1,3 @@
-function createWorkStationDiv(workstation, room) {
-
-    //
-    // For bootstrap grid system with breakpoints:
-    // 'col-xxl-2', 'col-xl-2', 'col-lg-2', 'col-md-3', 'col-sm-4', 'col-6', 'g-2'
-    // Add the above line to the classList below
-    //
-    // New Main div
-    let newDivMain = document.createElement('div');
-    newDivMain.classList.add('col', 'Room_' + room); newDivMain.style.width = '25%';
-    newDivMain.id = workstation;
-    newDivMain.style.display = 'block'; // style block for hiding by default!
-
-    // New Card for the workstation
-    let DivCard = document.createElement('div');
-    DivCard.classList.add('card'); DivCard.style.backgroundColor = `#4a5766`;
-    DivCard.id = workstation + "_card"; DivCard.style.borderColor = `#ff0000`;
-
-    // New div for the card body
-    let CardBody = document.createElement('div');
-    CardBody.classList.add('card-body'); CardBody.style.color = `#4a5766`;
-    //Title for which workstation
-    let PCTitle = document.createElement('h4');
-    PCTitle.textContent = `Workstation: ${workstation.split('.')[0]}`;
-    PCTitle.classList.add('card-title'); PCTitle.style.color = `#f9f9f9`; //PCTitle.style.width = '70%';
-    // Element for the current load on card (without opening the modal)
-    let currentLoadDirect = document.createElement('p');
-    currentLoadDirect.classList.add('card-text'); currentLoadDirect.textContent = 'Updating...';
-    currentLoadDirect.id = workstation + "_loadDirect";
-
-    // COLOR STUFF - DEFAULT: EVERYTHING IS OFFLINE / RED
-    // Element for the status title
-    let StatusTitle = document.createElement('p');
-    StatusTitle.classList.add('card-text');StatusTitle.style.color = `#f9f9f9`;
-    StatusTitle.textContent = "Room " +  room;
-    // status element offline/online -> red/green
-    let statusTextObject = document.createElement('span');
-    statusTextObject.textContent = " OFFLINE"; statusTextObject.classList.add("status");
-    statusTextObject.id = workstation + "_status"; statusTextObject.style.color = `#ff0000`;
-    // Logo element for aesthetics
-    let logoImageObject = document.createElement("img");
-    logoImageObject.id = workstation + "_img";
-    logoImageObject.classList.add("logo"); logoImageObject.style.height = `40px`;
-    logoImageObject.style.width = `50px`; logoImageObject.style.float = `right`;
-    logoImageObject.style.position = `relative`; logoImageObject.style.bottom = `60px`;
-    logoImageObject.setAttribute("src", "../../images/logo_OFFLINE.png");
-
-    // Create the modal for the workstation
-    let values = createModal(workstation, room);
-    let button = values[0];
-    let modal = values[1];
-
-    // Append everything together
-    newDivMain.appendChild(DivCard);
-    DivCard.appendChild(CardBody);
-    CardBody.appendChild(PCTitle);
-    CardBody.appendChild(StatusTitle);
-    CardBody.appendChild(logoImageObject);
-    // button from modal
-    CardBody.appendChild(button);
-    StatusTitle.appendChild(statusTextObject);
-    StatusTitle.appendChild(currentLoadDirect);
-    //newDivMain.appendChild(button);
-    DivCard.appendChild(modal);
-
-    return newDivMain;
-    // let serversDiv = document.getElementById("innerdiv");
-    // serversDiv.appendChild(newDivMain);
-}
-
-function createModal(workstation, room) {
-    // Show Status Button for the card
-    let button = document.createElement('button');
-    button.className = 'btn btn-primary';
-    button.setAttribute('data-bs-toggle', 'modal');
-    button.setAttribute('data-bs-target', '#reg-modal_' + workstation.split('.')[0]);
-    button.textContent = 'Show Status';
-
-    // Main Model DIV
-    let modal = document.createElement('div');
-    modal.className = 'modal'; modal.id = 'reg-modal_' + workstation.split('.')[0];
-    modal.tabIndex = '-1';
-    // Create modal dialog
-    let modalDialog = document.createElement('div');
-    modalDialog.className = 'modal-dialog';
-    // Create modal content
-    const modalContent = document.createElement('div');
-    modalContent.className = 'modal-content';
-
-    // Create the modal header, body and footer
-    let modalHeader = createModalHeader(workstation);
-    let modalBody = createModalBody(workstation);
-    let modalFooter = createModalFooter(workstation);
-
-    // Append everything together
-    modalContent.appendChild(modalHeader);
-    modalContent.appendChild(modalBody);
-    modalContent.appendChild(modalFooter);
-    modalDialog.appendChild(modalContent);
-    modal.appendChild(modalDialog);
-
-    // Return the button and the modal for the cards
-    return [button, modal]
-}
-
-function createModalHeader(workstation) {
-    // Create modal header
-    let modalHeader = document.createElement('div');
-    modalHeader.className = 'modal-header';
-    // Header Title
-    let title = document.createElement('h1');
-    title.className = 'modal-title';
-    title.textContent = 'Status of ' + workstation.split('.')[0];
-    // Header Close Button
-    let closeButton = document.createElement('button');
-    closeButton.className = 'btn-close';
-    closeButton.type = 'button';
-    closeButton.setAttribute('data-bs-dismiss', 'modal');
-
-    // Append title and close button to header
-    modalHeader.appendChild(title);
-    modalHeader.appendChild(closeButton);
-
-    return modalHeader;
-}
-
-function createModalBody(workstation) {
-    // Create modal body div
-    let modalBody = document.createElement('div');
-    modalBody.className = 'modal-body';
-    // Element for the current load
-    let currentLoad = document.createElement('p');
-    currentLoad.textContent = 'Updating...'; currentLoad.id = workstation + "_load";
-    // Element for the current free memory
-    let currentFreeMemory = document.createElement('p');
-    currentFreeMemory.textContent = 'Updating...'; currentFreeMemory.id = workstation + "_currentFreeMemory";
-    // Element for the load of the last 5 minutes
-    let loadLast5 = document.createElement('p');
-    loadLast5.textContent = 'Updating..'; loadLast5.id = workstation + "_loadlast5";
-    // Element for the available memory
-    let availableMemory = document.createElement('p');
-    availableMemory.textContent = 'Updating..'; availableMemory.id = workstation + "_availableMemory";
-    // Element for the temperature
-    let temperature = document.createElement('p');
-    temperature.textContent = 'Updating...'; temperature.id = workstation + "_temperature";
-
-    // Append all elements to modal body
-    modalBody.appendChild(currentLoad);
-    modalBody.appendChild(currentFreeMemory);
-    modalBody.appendChild(loadLast5);
-    modalBody.appendChild(temperature);
-    modalBody.appendChild(availableMemory);
-
-    return modalBody;
-}
-
-function createModalFooter() {
-    // Create a close button for the footer
-    let closeButtonModal = document.createElement('button');
-    closeButtonModal.className = 'btn btn-primary'; closeButtonModal.type = 'button';
-    closeButtonModal.setAttribute('data-bs-dismiss', 'modal');
-    closeButtonModal.textContent = 'Close';
-    // Main div for the footer
-    let modalFooter = document.createElement('div');
-    modalFooter.className = 'modal-footer';
-
-    // Append close button to footer
-    modalFooter.appendChild(closeButtonModal);
-
-    return modalFooter;
-}
 
 function mapDiv() {
     // Create the necessary elements
@@ -226,19 +55,78 @@ function getAllPCs(selectedRoom, data) {
 }
 
 
-// function getAllPCs(selectedRoom, data) {
-//     let servers = data.data.room[selectedRoom]
-//     if (serverState.currentServerIndex < servers.pc.length) {
-//         let server = servers.pc[serverState.currentServerIndex];
-//         serverState.currentServerIndex++;
-//
-//         return server;
-//     } else {
-//         // Move to the next room and reset the server index
-//         serverState.currentServerIndex = 0;
-//     }
-//
-//     // No more servers left
-//     return null;
-// }
+async function updateElement(selectedRoom) {
 
+    try {
+        let response = await fetch("data/config.json");
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let data = await response.json();
+        let searchRoom = data.data.room[selectedRoom];
+        let mapDiv = document.getElementById("mapdiv");
+
+        // serversDiv.innerHTML = ''; // Clear the div
+        mapDiv.classList.add('grid-container');
+        mapDiv.style.width = '80%';
+        mapDiv.style.margin = 'auto';
+
+
+        let serverInfo = getAllPCs(selectedRoom, data);
+
+        for (let row of searchRoom.classRoomMatrix) {
+            // console.log(row);
+            for (let cell of row) {
+                let newDivMain = document.createElement('div');
+                newDivMain.classList.add('col');
+                newDivMain.style.width = '16%';
+                for (let pcLabel of serverInfo) {
+                    if (cell === pcLabel.split('.')[0]) {
+                        let serverDiv = smallDiv(pcLabel);
+                        newDivMain.appendChild(serverDiv);
+                    }
+                    mapDiv.appendChild(newDivMain);
+                }
+            }
+        }
+    } catch (error) {
+        console.error('Error fetching config data: ', error);
+    }
+}
+
+
+
+function handleCheckboxInteraction(checkbox) {
+    let cardID = 'Room_' + checkbox.id;
+    let divs = document.getElementsByClassName("col " + cardID);
+
+    let mapID = document.getElementById("Map");
+    let mapdiv = document.getElementById("mapdiv");
+
+    document.querySelector('footer').classList.add("position-static");
+
+    if (mapID.checked === true && checkbox.checked === true) {
+        for (let div of divs) {
+            div.style.display = 'none';
+        }
+        // empty mapdiv if exists
+        if (mapdiv) {mapdiv.innerHTML = '';}
+        // create mapDiv object
+        mapDiv();
+        // create map child divs
+        updateElement(checkbox.id);
+        document.querySelector('footer').classList.remove("position-static");
+        document.querySelector('footer').classList.add("fixed-bottom");
+    }
+
+    if (mapID.checked === false || checkbox.checked === false) {
+        if (mapdiv) {mapdiv.innerHTML = '';}
+        for (let div of divs) {
+            if (checkbox.checked === true) {
+                div.style.display = 'block';
+            } else {
+                div.style.display = 'none';
+            }
+        }
+    }
+}
--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,95 +1,53 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const mapCheckbox = document.getElementById("Map");
-    const roomCheckboxes = document.querySelectorAll("input[type='checkbox'][name='room']");
-    const mapDiv = document.getElementById("mapdiv");
-
-    function updateCheckboxStates() {
-        const checkedRoomCount = Array.from(roomCheckboxes).filter(cb => cb.checked).length;
-        const isMapChecked = mapCheckbox.checked;
-
-        // Disable map checkbox if two or more rooms are selected
-        mapCheckbox.disabled = checkedRoomCount >= 2;
-
-        // Enable or disable room checkboxes based on the map checkbox and the number of checked rooms
-        roomCheckboxes.forEach(cb => {
-            if (isMapChecked && checkedRoomCount === 1) {
-                cb.disabled = !cb.checked;
-            } else {
-                cb.disabled = false;
-            }
-        });
-
-        // Show or hide map based on the map checkbox and the number of checked rooms
-        mapDiv.style.display = (isMapChecked && checkedRoomCount === 1) ? 'block' : 'none';
-    }
-
-    // Add event listeners to checkboxes
-    mapCheckbox.addEventListener('change', updateCheckboxStates);
-    roomCheckboxes.forEach(cb => {
-        cb.addEventListener('change', updateCheckboxStates);
-    });
-
-    // Initial state update
-    updateCheckboxStates();
-});
-
-
-
-async function retrieveData() {
-    let response = await fetch("data/config.json");
-    let data = await response.json();
-
-    let rooms = Object.entries(data.data.room);
-    return rooms.reduce((acc, [room, roomData]) => {
-        let pcs = roomData.pc;
-        pcs.forEach(pc => acc.push({workstation: pc, room: room}));
-        return acc;
-    }, []);
-}
-
-function handling() {
-    retrieveData().then(AllPCS => {
-        let innerdiv = document.getElementById("innerdiv");
-
-        for (let i = 0; i < AllPCS.length; i++) {
-            let workstation = AllPCS[i].workstation;
-            let room = AllPCS[i].room;
-            let newDivMain = createWorkStationDiv(workstation, room);
-            innerdiv.appendChild(newDivMain);
-        }
-    });
-}
-
-function showHideAllElements(status) {
-    let divs = document.querySelectorAll("[class^='col Room_']");
-    divs.forEach(function(div) {
-        div.style.display = status;
-    });
-}
-
-// Initial function call to show all elements
-
-let checkboxes = document.querySelectorAll("input[type='checkbox']");
-checkboxes.forEach(function(checkbox) {
-    checkbox.addEventListener('change', function() {
-        checkboxes.forEach(function(checkbox) {
-            // function that handles visibility of cards or map div objects
-            handleCheckboxInteraction(checkbox);
-        });
-        let status = [];
-        checkboxes.forEach(function(checkbox) {
-            status.push(checkbox.checked);
-        });
-        if (!status.includes(true)) {
-            showHideAllElements('block');
-        }
+// get all the buttons with a querySelectorAll on type = button
+let buttons = document.querySelectorAll("button[type=button]");
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        let buttonID = button.id;
+        console.log("BUTTON CLICKED: " + buttonID);
+        void handleMap(buttonID);
     });
 });
 
+async function handleMap(room) {
+    let configFile = await fetch("data/config.json");
+    let configData = await configFile.json();
 
-setInterval(function () {
-    void handlingUpdate();
-}, 2500);
+    let workstations = getAllWorkstations(configData, room);
+    configData.data.room[room].classRoomMatrix.forEach(function (row) {
+       row.forEach(function (cell) {
+           workstations.forEach(function (workstation) {
+              console.log(cell + " " + workstation);
+           });
 
-// Calling the handling function, that starts the whole process
-handling();
+       });
+    });
+
+    /////////////////////////////////////////////
+    /////////////////////////////////////////////
+
+    // for (let row of configData.data.room[room].classRoomMatrix) {
+    //     // console.log(row);
+    //     for (let cell of row) {
+    //         for (let pcLabel of workstations) {
+    //             console.log(cell + " " + pcLabel.split('.')[0]);
+    //             if (cell === pcLabel.split('.')[0]) {
+    //                 console.log("FOUND: " + pcLabel.split('.')[0]);
+    //             }
+    //         }
+    //     }
+    // }
+
+    /////////////////////////////////////////////
+    /////////////////////////////////////////////
+
+}
+
+function getAllWorkstations(configData, room) {
+    let workstations = [];
+    let roomData = configData.data.room[room];
+    roomData.pc.forEach(function (workstation) {
+        workstations.push(workstation);
+    })
+
+    return workstations;
+}
