072ec0bf06b5c26ceecfdda77d715aa773d59949
mvandestreek <m.van.de.streek@st.hanze.nl>
2024-01-15 10:53:07 +0100

    created new servlet for map

---
+++ src/main/java/nl/bioinf/shbreekers/servlets/MapServlet.java
@@ -0,0 +1,41 @@
+package nl.bioinf.shbreekers.servlets;
+
+import nl.bioinf.shbreekers.config.WebConfig;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "MapServlet", urlPatterns = "/map", loadOnStartup = 1)
+public class MapServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Initializing Thymeleaf template engine");
+        final ServletContext servletContext = this.getServletContext();
+        WebConfig.createTemplateEngine(servletContext);
+    }
+    private static final long serialVersionUID = 1L;
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        process(request, response);
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        process(request, response);
+    }
+    public void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+            WebConfig.configureResponse(response);
+            WebContext ctx = new WebContext(
+                    request,
+                    response,
+                    request.getServletContext(),
+                    request.getLocale());
+
+            WebConfig.createTemplateEngine(getServletContext()).
+                    process("Map", ctx, response.getWriter());
+        }
+}
