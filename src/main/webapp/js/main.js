4e1ece2840fedc890f7902ef27cc50bfe902a1dc
Coldbirdie <60214213+Coldbirdie@users.noreply.github.com>
2024-01-22 15:47:35 +0100

    Create separate js file for scrollbutton since it's used in multiple pages and it's unnecessary to call main.js in Map.html

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -32,32 +32,6 @@ function showHideAllElements(status) {
     });
 }
 
-let scrollButton = document.getElementById("btn-page-up");
-
-// Start show button event after scroll
-window.onscroll = function () {
-    showScrollButton();
-};
-
-function showScrollButton() {
-    if (
-        document.body.scrollTop > 10 ||
-        document.documentElement.scrollTop > 10
-    ) { // causes the button to show after performing 1 scroll
-        scrollButton.style.display = "block";
-    } else {
-        // causes the button to disappear when at top of page
-        scrollButton.style.display = "none"
-    }
-}
-
-// Start scroll up when on-click event is activated
-scrollButton.addEventListener("click", scrollPageUp);
-
-function scrollPageUp() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
 
 let checkboxes = document.querySelectorAll("input[type='checkbox']");
 checkboxes.forEach(function(checkbox) {
